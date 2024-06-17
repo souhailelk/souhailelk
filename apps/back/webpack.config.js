f49504d6b0e1e02e4071f21bf43a51c6aada1f04
souhailelk <elkaissi.souhail.me@gmail.com>
2024-06-18 00:06:35 +0100

    [back] Change output path to /back/api

--- apps/back/webpack.config.js
+++ apps/back/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/back'),
+    path: join(__dirname, '../../dist/apps/back/api'),
   },
   plugins: [
     new NxAppWebpackPlugin({
