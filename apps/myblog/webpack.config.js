d1cb3eb38e068dc6babb3e12cf221c673f79f569
souhailelk <elkaissi.souhail.me@gmail.com>
2024-06-17 17:25:36 +0100

    [myblog] Fix logo

--- apps/myblog/webpack.config.js
+++ apps/myblog/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
       main: './src/main.tsx',
       index: './src/index.html',
       baseHref: '/',
-      assets: ['./src/favicon.ico', './src/assets'],
+      assets: ['./src/mylogo.ico', './src/assets'],
       styles: ['./src/styles.css'],
       outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
       optimization: process.env['NODE_ENV'] === 'production',
