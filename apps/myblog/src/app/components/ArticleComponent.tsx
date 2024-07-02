583bdb1a28f5150454904251ed00cf49d80d2888
souhailelk <elkaissi.souhail.me@gmail.com>
2024-07-02 01:36:01 +0100

    [myblog] Fix cover image size

--- apps/myblog/src/app/components/ArticleComponent.tsx
+++ apps/myblog/src/app/components/ArticleComponent.tsx
@@ -44,7 +44,7 @@ function ArticleComponent() {
 
     <div className="flex justify-center overflow-hidden rounded">
       <div className="w-full h-full max-w-screen-xl shadow-lg px-6 py-4 overflow-auto">
-        <img alt="Article" className="object-cover h-1/2 w-full" src={article.mainImageUrl} />
+        <img alt="Article" className="w-full h-96 md:h-[600px] lg:h-[800px] object-cover" src={article.mainImageUrl} />
         <div className="font-bold justify-center text-2xl mm:text-4xl break-words">{article.title}</div>
         <div className="text-sm md:text-lg tracking-widest break-words ml-3 mb-3">published on {format(article.date, 'MMMM do, yyyy H:mm:ss a')}</div>
         <div className="text-gray-700 text-xs md:text-base leading-relaxed text-justify font-mono break-words px-2 py-6"><div dangerouslySetInnerHTML={{ __html: article.content }} /></div>
