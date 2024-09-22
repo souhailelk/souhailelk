de28b56fcebf20a61e6aa79fa14192038bd23b80
souhailelk <elkaissi.souhail.me@gmail.com>
2024-09-22 22:19:08 +0100

    [Article] Fix cover image css

--- apps/myblog/src/app/components/ArticleComponent.tsx
+++ apps/myblog/src/app/components/ArticleComponent.tsx
@@ -42,9 +42,9 @@ function ArticleComponent() {
       tags.push(<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs md:text-sm font-semibold text-gray-700 mr-2 mt-3">#{tag}</span>));
   return (
 
-    <div className="flex justify-center overflow-hidden rounded">
-      <div className="w-full h-full max-w-screen-xl shadow-lg px-6 py-4 overflow-auto">
-        <img alt="Article" className="w-full h-96 md:h-[600px] lg:h-[800px] object-cover" src={article.mainImageUrl} />
+    <div className="flex justify-center overflow-hidden px-6 py-4">
+      <div className=" w-full h-full max-w-screen-xl shadow-lg overflow-auto">
+        <img alt="Article" className="w-full h-[100px] md:h-[300px] lg:h-[400px] object-cover" src={article.mainImageUrl} />
         <div className="font-bold justify-center text-6xl mm:text-4xl">{article.title}</div>
         <div className="text-sm md:text-lg tracking-widest break-words ml-3 mb-3">published on {format(article.date, 'MMMM do, yyyy H:mm:ss a')}</div>
         <div className="text-gray-700 text-xs md:text-base leading-relaxed text-justify font-mono break-words px-2 py-6" dangerouslySetInnerHTML={{ __html: article.content }}></div>
